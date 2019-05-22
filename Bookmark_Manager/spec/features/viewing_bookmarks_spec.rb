d2c1382f6b19515d90e8e4f0067abb810f063354
Tenzin <tenzinball@icloud.com>
2019-05-22 11:26:37 +0100

    postgresql installation

--- Bookmark_Manager/spec/features/viewing_bookmarks_spec.rb
+++ Bookmark_Manager/spec/features/viewing_bookmarks_spec.rb
@@ -4,3 +4,12 @@
       expect(page).to have_content "Bookmark Manager"
     end
   end
+
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/bookmarks')
+      expect(page).to have_content "http://www.makersacademy.com"
+      expect(page).to have_content "http://www.destroyallsoftware.com"
+      expect(page).to have_content "http://www.google.com"
+    end
+  end
