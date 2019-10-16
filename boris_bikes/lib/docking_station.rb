6718991fe4d8b1a7f6a1dd414a53fbc2ddebdeb1
Tenzin <tenzinball@icloud.com>
2019-10-16 17:01:50 +0100

    end of raising exceptions

--- boris_bikes/lib/docking_station.rb
+++ boris_bikes/lib/docking_station.rb
@@ -1,9 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+    attr_reader :bike
 
-  def release_bike
-    Bike.new
+    def release_bike
+     fail 'No bikes available' unless @bike
+     @bike
+   end
+
+  def dock(bike)
+      @bike = bike
   end
 
 end
--- boris_bikes/spec/docking_station_spec.rb
+++ boris_bikes/spec/docking_station_spec.rb
@@ -4,7 +4,31 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
-end
+  describe '#release_bike' do
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        # we want to release the bike we docked
+        expect(subject.release_bike).to eq bike
+      end
+      it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+    end
+  #it { is_expected.to respond_to(:dock).with(1).argument }
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+  #it { is_expected.to respond_to(:bike)}
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+  end
